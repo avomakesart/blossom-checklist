4f0a2ae5de202a40fa033a9bbdb13b4b18d06947
Alvaro Castillo <acastillocorrea@bol.com>
2024-07-18 19:52:51 +0200

    BLOSSOM: adding formular

---
+++ blossom-checklist.rb
@@ -0,0 +1,15 @@
+class Checklist < Formula
+  desc "A pre-push checklist tool"
+  homepage "https://github.com/avomakesart/homebrew-blossom-checklist"
+  url "https://github.com/avomakesart/homebrew-blossom-checklist/archive/refs/tags/blossom-first.tar.gz"
+  sha256 "336063f228b5bda3fd315350a7b47466527ded221c315c973d6012057a324b4d"
+  version "1.0.0"
+
+  def install
+    bin.install "blossom-checklist"
+  end
+
+  test do
+    system "#{bin}/blossom-checklist", "--version"
+  end
+end
