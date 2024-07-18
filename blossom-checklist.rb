9a490549faccb5a37ccb3213e363c869974ad2cf
Alvaro Castillo <acastillocorrea@bol.com>
2024-07-18 22:51:46 +0200

    BLOSSOM: fix

--- blossom-checklist.rb
+++ blossom-checklist.rb
@@ -3,9 +3,12 @@ class Checklist < Formula
   homepage "https://github.com/avomakesart/homebrew-blossom-checklist"
   url "https://github.com/avomakesart/homebrew-blossom-checklist/archive/refs/tags/blossom-first.tar.gz"
   sha256 "336063f228b5bda3fd315350a7b47466527ded221c315c973d6012057a324b4d"
-  version "1.0.0"
+  version "1.0.1"
+
+  depends_on "go" => :build
 
   def install
+    system "go", "build", "-o", "checklist"
     bin.install "blossom-checklist"
   end
 
